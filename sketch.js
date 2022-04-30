ab3a6e7af3da99c57f4de221761560ca60aa2584
Lablack Mourad <mouradost@gmail.com>
2022-05-01 02:05:57 +0800

    update

--- sketch.js
+++ sketch.js
@@ -10,22 +10,30 @@ function setup() {
   canvas.parent("canvasContainer");
   frameRate(30);
   start();
-  button = createButton('submit');
+  button = createButton('Restart');
   button.mousePressed(start);
 }
 
 function draw() {
-  background(10, 100, 100, 255);
+  background(94, 94, 94);
   actors.forEach(actor => {
-    // actor.move(velocity = 10);
+    actor.move(velocity = random(0, 5));
     actor.grow(size=5);
     actor.show();
+    actors.forEach(otherActor => {
+      if (actor.name != otherActor.name){
+      let overlap = actor.isOverlapping(otherActor.x, otherActor.y, otherActor.r);
+      if (overlap) {
+        actor.strokeColor = color(0, 0, 0);
+        actor.color = color(59, 19, 168, 20);
+      }}
+    })
   });
 }
 
 function start() {
   actors = [];
-  createActors(nb = 100, r = 1);
+  createActors(nb = 10, r = 1);
 }
 
 function windowResized() {
@@ -38,29 +46,30 @@ function createActors(nb, r) {
   for (let i = 1; i < nb + 1; i++) {
     let x = random((sketchWidth/8), (sketchWidth/1.5));
     let y = random((sketchHeight/8), (sketchHeight/1.5));
-    actors.push(new Actor(x, y, r));
+    actors.push(new Actor(x, y, r, i));
   }
 }
 
 class Actor {
-  constructor(x, y, r) {
+  constructor(x, y, r, index) {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.name = "Actor " + index;
+    this.strokeColor = color(59, 19, 168);
+    this.color = color(59, 19, 168, 200);
   }
 
   move(velocity) {
-
-    if (this.isOut(velocity, velocity)) {
-      velocity *= -1;
+    if (!this.isOut(velocity, velocity)) {
+      this.x += (1 * velocity);
+      this.y += (1 * velocity);
     }
-    this.x = this.x + (1 * velocity);
-    this.y = this.y + (1 * velocity);
   }
 
   grow(size) {
     if (!this.isOut(size, size)) {
-      this.r = this.r + size;
+      this.r += size;
     }
     
   }
@@ -74,11 +83,16 @@ class Actor {
     )
   }
 
+  isOverlapping(x, y, r) {
+    return (dist(this.x, this.y, x, y) < (r + this.r))
+  }
+
   show() {
-    stroke(177, 177, 177);
-    strokeWeight(5);
-    // fill(200, 200, 200);
-    noFill()
+    // stroke(this.strokeColor);
+    // strokeWeight(5);
+    noStroke();
+    fill(this.color);
+    // noFill();
     ellipse(this.x, this.y, this.r*2, this.r*2);
   }
 }
